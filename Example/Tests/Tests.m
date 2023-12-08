dcd14dbca745be2f8fefaf88eb66b394ffa069ac
Mohammad Ashraf Ali <ashraf@notifyvisitors.com>
2023-12-08 16:25:27 +0530

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  nv_framework_testTests.m
+//  nv_framework_testTests
+//
+//  Created by ashraf-nv on 12/08/2023.
+//  Copyright (c) 2023 ashraf-nv. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
