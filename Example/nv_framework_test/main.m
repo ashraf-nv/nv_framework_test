590783ec68461d2c49b011288feef8602d71b865
Mohammad Ashraf Ali <ashraf@notifyvisitors.com>
2023-12-09 12:21:39 +0530

    Initial commit

---
+++ Example/nv_framework_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nv_framework_test
+//
+//  Created by ashraf-nv on 12/09/2023.
+//  Copyright (c) 2023 ashraf-nv. All rights reserved.
+//
+
+@import UIKit;
+#import "NV_FRAMEWORK_TESTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NV_FRAMEWORK_TESTAppDelegate class]));
+    }
+}
