e83af380438fee521f027530b2fb580972c7e7a0
Mohammad Ashraf Ali <ashraf@notifyvisitors.com>
2023-12-09 15:42:27 +0530

    Initial commit

---
+++ Example/nv_framework_test/NV_FRAMEWORK_TESTViewController.h
@@ -0,0 +1,13 @@
+//
+//  NV_FRAMEWORK_TESTViewController.h
+//  nv_framework_test
+//
+//  Created by ashraf-nv on 12/09/2023.
+//  Copyright (c) 2023 ashraf-nv. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NV_FRAMEWORK_TESTViewController : UIViewController
+
+@end
---
+++ Example/nv_framework_test/NV_FRAMEWORK_TESTViewController.m
@@ -0,0 +1,29 @@
+//
+//  NV_FRAMEWORK_TESTViewController.m
+//  nv_framework_test
+//
+//  Created by ashraf-nv on 12/09/2023.
+//  Copyright (c) 2023 ashraf-nv. All rights reserved.
+//
+
+#import "NV_FRAMEWORK_TESTViewController.h"
+
+@interface NV_FRAMEWORK_TESTViewController ()
+
+@end
+
+@implementation NV_FRAMEWORK_TESTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
